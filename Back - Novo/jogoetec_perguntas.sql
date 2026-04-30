66ba91564e7fb655c9df26d46278a64f1a976b55
ArthurPratesLopes <162491453+ArthurPratesLopes@users.noreply.github.com>
2026-04-30 06:59:28 -0300

    Add files via upload

---
+++ Back - Novo/jogoetec_perguntas.sql
@@ -0,0 +1,18 @@
+INSERT INTO perguntas (enunciado,Opcao_A,Opcao_B, Opcao_C, Opcao_D, CORRETA, Dificuldade,Pontuacao , DICA) VALUES
+-- FÁCEIS (10 pontos)
+('Qual o símbolo químico do Oxigênio?', 'Ox', 'O', 'Og', 'On', 'B', 'Fácil', 10, 'É apenas uma letra.'),
+('Qual instrumento é usado para medir volumes precisos de líquidos?', 'Proveta', 'Béquers', 'Tubo de ensaio', 'Espátula', 'A', 'Fácil', 10, 'Tem um formato cilíndrico e graduado.'),
+('Qual o estado físico da água a 25°C?', 'Sólido', 'Gasoso', 'Líquido', 'Plasmático', 'C', 'Fácil', 10, 'É como ela sai da torneira.'),
+('Qual o PH de uma solução neutra?', '0', '14', '5', '7', 'D', 'Fácil', 10, 'É o valor central da escala.'),
+
+-- MÉDIOS (20 pontos)
+('Qual destes elementos é um metal alcalino?', 'Ferro', 'Cálcio', 'Sódio', 'Alumínio', 'C', 'Médio', 20, 'Está na primeira coluna da tabela periódica.'),
+('O que representa a fórmula H2SO4?', 'Ácido Nítrico', 'Ácido Sulfúrico', 'Ácido Clorídrico', 'Água Oxigenada', 'B', 'Médio', 20, 'É um ácido forte usado em baterias.'),
+('Quem é considerado o pai da Química moderna?', 'Einstein', 'Newton', 'Lavoisier', 'Dalton', 'C', 'Médio', 20, 'Na natureza nada se cria, nada se perde...'),
+('O que acontece em uma reação de combustão?', 'Resfriamento', 'Ganho de elétrons', 'Liberação de calor e luz', 'Solidificação', 'C', 'Médio', 20, 'Pense no fogo.'),
+
+-- DIFÍCEIS (30 pontos)
+('Qual a geometria molecular da água (H2O)?', 'Linear', 'Angular', 'Trigonal Plana', 'Tetraédrica', 'B', 'Difícil', 30, 'Pense no par de elétrons livres do Oxigênio.'),
+('Qual o nome da vidraria usada especificamente para titulações?', 'Pipeta', 'Cadinho', 'Bureta', 'Funil de Buchner', 'C', 'Difícil', 30, 'Possui uma torneira na ponta.'),
+('Qual o elemento químico com o maior número atômico natural?', 'Urânio', 'Plutônio', 'Ouro', 'Chumbo', 'A', 'Difícil', 30, 'É um elemento radioativo com número 92.'),
+('O que define uma ligação covalente?', 'Transferência de elétrons', 'Compartilhamento de elétrons', 'Atração entre íons', 'Forças de Van der Waals', 'B', 'Difícil', 30, 'Ocorre entre ametais.');
